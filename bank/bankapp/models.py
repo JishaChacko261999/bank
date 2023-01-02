346c21add7ce0794a61acca69d2bc578390f3deb
JishaChacko261999 <jishachacko26@gmail.com>
2023-01-02 11:44:27 +0530

    commit

---
+++ bank/bankapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Services(models.Model):
+    name=models.CharField(max_length=250)
+    desc=models.TextField()
+    img=models.ImageField(upload_to='service')
+
+    def __str__(self):
+        return self.name
+
+class Form(models.Model):
+    name=models.CharField(max_length=250)
+    dob=models.DateField()
+    age=models.IntegerField()
+    gender=models.CharField(max_length=250)
+    phone=models.CharField(max_length=10)
+    email=models.EmailField()
+    address=models.TextField()
+    district=models.CharField(max_length=250)
+    branch=models.CharField(max_length=250)
+    account_type=models.CharField(max_length=250)
+    materials_provide=models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
